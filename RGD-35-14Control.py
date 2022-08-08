a818a0a57b4046b68b45ae67998dc13b888a9430
SUSTechJY <12132265@mail.sustech.edu.com>
2022-08-08 22:55:35 +0800

    bytes

--- RGD-35-14Control.py
+++ RGD-35-14Control.py
@@ -1,7 +1,11 @@
+'''
+
+'''
 import serial
 import time
 import crcmod
 import threading
+from tqdm import *
 
 close_cmd = [0xA0, 0x01, 0x01, 0xA2]
 
@@ -14,8 +18,10 @@ class ComSwitch(object):
     def calCrc(self, array):
         bytes_ = b''
         for i in range(array.__len__()):
-            bytes_ = bytes_ + array[i].to_bytes(1, byteorder='little')
+            bytes_ = bytes_ + array[i].to_bytes(1, byteorder='little', signed=True)
         crc = hex(self.crc16(bytes_))
+        print(bytes_)
+        print(crc)
         crcQ = '0x' + crc[-2] + crc[-1]
         crcH = '0x' + crc[-4] + crc[-3]
         return int(crcQ.encode(), 16), int(crcH.encode(), 16)
@@ -79,24 +85,6 @@ class ComSwitch(object):
         self.sc.write(setPositionCmd)
         print(self.readSerial())
 
-    # 设置旋转位置
-    def setRotation(self, Rotation):
-        RotationHex = hex(Rotation)
-        if RotationHex.__len__() <= 4:
-            array = [0x01, 0x06, 0x01, 0x09, 0x00, Rotation]
-            currentRotationQ, currentRotationH = self.calCrc(array)
-            setRotationCmd = [0x01, 0x06, 0x01, 0x09, 0x00, Rotation, currentRotationQ, currentRotationH]
-        else:
-            RotationHex = hex(Rotation)
-            RotationHexQ = int(RotationHex[0] + RotationHex[1] + RotationHex[2], 16)
-            RotationHexH = int(RotationHex[0] + RotationHex[1] + RotationHex[-2] + RotationHex[-1], 16)
-            array = [0x01, 0x06, 0x01, 0x09, RotationHexQ, RotationHexH]
-            currentRotationQ, currentRotationH = self.calCrc(array)
-            setRotationCmd = [0x01, 0x06, 0x01, 0x09, RotationHexQ, RotationHexH, currentRotationQ,
-                              currentRotationH]
-        self.sc.write(setRotationCmd)
-        print(self.readSerial())
-
     # 初始化反馈
     def initFeedback(self):
         cmd = [0x01, 0x03, 0x02, 0x00, 0x00, 0x01, 0x85, 0xB2]
@@ -120,16 +108,104 @@ class ComSwitch(object):
         self.sc.write(cmd)
         back = self.readSerial()
 
+    # 设置相对旋转位置
+    def setRelativeRotation(self, Rotation):
+        if Rotation < 0:
+            RotationHex = hex(Rotation)
+            if RotationHex.__len__() <= 5:
+                array = [0x01, 0x06, 0x01, 0x09, 0x00, Rotation]
+                currentRotationQ, currentRotationH = self.calCrc(array)
+                setRotationCmd = [0x01, 0x06, 0x01, 0x09, 0x00, Rotation, currentRotationQ, currentRotationH]
+            else:
+                RotationHex = hex(Rotation)
+                RotationHexQ = int(RotationHex[0] + RotationHex[1] + RotationHex[2] + RotationHex[3], 16) - 1
+                RotationHexH = int(RotationHex[0] + RotationHex[1] + RotationHex[2] + RotationHex[-2] + RotationHex[-1],
+                                   16) - 1
+                array = [0x01, 0x06, 0x01, 0x09, RotationHexQ, RotationHexH]
+                currentRotationQ, currentRotationH = self.calCrc(array)
+                setRotationCmd = [0x01, 0x06, 0x01, 0x09, RotationHexQ, RotationHexH, currentRotationQ,
+                                  currentRotationH]
+        else:
+            RotationHex = hex(Rotation)
+            if RotationHex.__len__() <= 4:
+                array = [0x01, 0x06, 0x01, 0x09, 0x00, Rotation]
+                currentRotationQ, currentRotationH = self.calCrc(array)
+                setRotationCmd = [0x01, 0x06, 0x01, 0x09, 0x00, Rotation, currentRotationQ, currentRotationH]
+            else:
+                RotationHex = hex(Rotation)
+                RotationHexQ = int(RotationHex[0] + RotationHex[1] + RotationHex[2], 16)
+                RotationHexH = int(RotationHex[0] + RotationHex[1] + RotationHex[-2] + RotationHex[-1], 16)
+                array = [0x01, 0x06, 0x01, 0x09, RotationHexQ, RotationHexH]
+                currentRotationQ, currentRotationH = self.calCrc(array)
+                setRotationCmd = [0x01, 0x06, 0x01, 0x09, RotationHexQ, RotationHexH, currentRotationQ,
+                                  currentRotationH]
+        self.sc.write(setRotationCmd)
+        print(self.readSerial())
+
+    # 设置绝对旋转位置
+    def setAbsoluteRotation(self, Rotation):
+        if Rotation < 0:
+            RotationHex = hex(Rotation)
+            if RotationHex.__len__() <= 5:
+                array = [0x01, 0x06, 0x01, 0x05, 0x00, Rotation]
+                currentRotationQ, currentRotationH = self.calCrc(array)
+                setRotationCmd = [0x01, 0x06, 0x01, 0x05, 0x00, Rotation, currentRotationQ, currentRotationH]
+            else:
+                RotationHex = hex(Rotation)
+                RotationHexQ = int(RotationHex[0] + RotationHex[1] + RotationHex[2] + RotationHex[3], 16) - 1
+                RotationHexH = int(RotationHex[0] + RotationHex[1] + RotationHex[2] + RotationHex[-2] + RotationHex[-1],
+                                   16) - 1
+                array = [0x01, 0x06, 0x01, 0x05, RotationHexQ, RotationHexH]
+                currentRotationQ, currentRotationH = self.calCrc(array)
+                setRotationCmd = [0x01, 0x06, 0x01, 0x05,
+                                  int('0x' + (RotationHexQ.to_bytes(1, byteorder='big', signed=True).hex()), 16),
+                                  int('0x' + RotationHexH.to_bytes(1, byteorder='big', signed=True).hex(), 16),
+                                  currentRotationQ,
+                                  currentRotationH]
+
+        else:
+            RotationHex = hex(Rotation)
+            if RotationHex.__len__() <= 4:
+                array = [0x01, 0x06, 0x01, 0x05, 0x00, Rotation]
+                currentRotationQ, currentRotationH = self.calCrc(array)
+                setRotationCmd = [0x01, 0x06, 0x01, 0x05, 0x00, Rotation, currentRotationQ, currentRotationH]
+            else:
+                RotationHex = hex(Rotation)
+                RotationHexQ = int(RotationHex[0] + RotationHex[1] + RotationHex[2], 16)
+                RotationHexH = int(RotationHex[0] + RotationHex[1] + RotationHex[-2] + RotationHex[-1], 16)
+                array = [0x01, 0x06, 0x01, 0x05, RotationHexQ, RotationHexH]
+                currentRotationQ, currentRotationH = self.calCrc(array)
+                setRotationCmd = [0x01, 0x06, 0x01, 0x05, RotationHexQ, RotationHexH, currentRotationQ,
+                                  currentRotationH]
+        self.sc.write(setRotationCmd)
+        print(self.readSerial())
+
+    # 设置旋转速度
+    def setRotationVel(self, vel):
+        array = [0x01, 0x06, 0x01, 0x07, 0x00, vel]
+        crcQ, crcH = self.calCrc(array)
+        ForceCmd = [0x01, 0x06, 0x01, 0x07, 0x00, vel, crcQ, crcH]
+        self.sc.write(ForceCmd)
+        print(self.readSerial())
+
+    # 设置旋转力
+    def setRotationForce(self, Force):
+        array = [0x01, 0x06, 0x01, 0x08, 0x00, Force]
+        crcQ, crcH = self.calCrc(array)
+        ForceCmd = [0x01, 0x06, 0x01, 0x08, 0x00, Force, crcQ, crcH]
+        self.sc.write(ForceCmd)
+        print(self.readSerial())
+
 
 if __name__ == "__main__":
     cs = ComSwitch()
     cs.InitFinger()
     time.sleep(0.2)
-    for i in range(1000):
+    for i in tqdm(range(1000)):
         # cs.setForce(100)
-        cs.setRotation(0)
-        time.sleep(0.5)
+        cs.setAbsoluteRotation(-360)
+        time.sleep(1)
         # cs.setPosition(300)
         # cs.setForce(20)
-        cs.setRotation(1000)
-        time.sleep(1.5)
+        cs.setAbsoluteRotation(360)
+        time.sleep(1)
