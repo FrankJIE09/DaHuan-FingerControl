c7691986077c13d20da555bb557af7fc918411c6
SUSTechJY <12132265@mail.sustech.edu.com>
2022-08-09 15:04:46 +0800

    reset

---
+++ ControlRoot.py
@@ -0,0 +1,95 @@
+'''
+
+'''
+import serial
+import time
+import crcmod
+import threading
+from tqdm import *
+
+
+class ControlRoot(object):
+    def __init__(self, com="/dev/ttyUSB0"):
+        self.sc = serial.Serial(port=com, baudrate=115200)
+        self.crc16 = crcmod.mkCrcFun(0x18005, rev=True, initCrc=0xFFFF, xorOut=0x0000)
+
+    def calCrc(self, array):
+        bytes_ = b''
+        for i in range(array.__len__()):
+            bytes_ = bytes_ + array[i].to_bytes(1, byteorder='little', signed=True)
+        crc = hex(self.crc16(bytes_))
+        crcQ = '0x' + crc[-2] + crc[-1]
+        crcH = '0x' + crc[-4] + crc[-3]
+        return int(crcQ.encode(), 16), int(crcH.encode(), 16)
+
+    def readSerial(self):
+        # BTime = time.time()
+        time.sleep(0.008)
+        readContent = self.sc.read_all()
+        return readContent
+
+    def sendCmd(self, ModbusHighAddress, ModbusLowAddress, Value=0x01, isSet=True, isReadSerial=True):
+        if isSet:
+            SetAddress = 0x06
+        else:
+            SetAddress = 0x03
+        if Value < 0:
+            ValueHex = hex(Value)
+            if ValueHex.__len__() <= 5:
+                array = [0x01, SetAddress, ModbusHighAddress, ModbusLowAddress, 0x00, Value]
+                currentValueQ, currentValueH = self.calCrc(array)
+                setValueCmd = [0x01, SetAddress, ModbusHighAddress, ModbusLowAddress, 0x00, Value, currentValueQ,
+                               currentValueH]
+            elif ValueHex.__len__() == 6:
+                ValueHex = hex(Value)
+                ValueHexQ = int(ValueHex[0] + ValueHex[1] + ValueHex[2] + ValueHex[3], 16) - 1
+                ValueHexH = int(ValueHex[0] + ValueHex[1] + ValueHex[2] + ValueHex[-2] + ValueHex[-1],
+                                16) - 1
+                array = [0x01, SetAddress, ModbusHighAddress, ModbusLowAddress, ValueHexQ, ValueHexH]
+                currentValueQ, currentValueH = self.calCrc(array)
+                setValueCmd = [0x01, SetAddress, ModbusHighAddress, ModbusLowAddress,
+                               int('0x' + (ValueHexQ.to_bytes(1, byteorder='little', signed=True).hex()), 16),
+                               int('0x' + ValueHexH.to_bytes(1, byteorder='little', signed=True).hex(), 16),
+                               currentValueQ,
+                               currentValueH]
+            else:
+                ValueHex = hex(Value)
+                ValueHexQ = int(ValueHex[0] + ValueHex[1] + ValueHex[2] + ValueHex[3] + ValueHex[4],
+                                16) - 1
+                ValueHexH = int(ValueHex[0] + ValueHex[1] + ValueHex[2] + ValueHex[-2] + ValueHex[-1],
+                                16) - 1
+                array = [0x01, SetAddress, ModbusHighAddress, ModbusLowAddress, ValueHexQ, ValueHexH]
+                currentValueQ, currentValueH = self.calCrc(array)
+                setValueCmd = [0x01, SetAddress, ModbusHighAddress, ModbusLowAddress,
+                               int('0x' + (ValueHexQ.to_bytes(1, byteorder='little', signed=True).hex()), 16),
+                               int('0x' + ValueHexH.to_bytes(1, byteorder='little', signed=True).hex(), 16),
+                               currentValueQ,
+                               currentValueH]
+
+        else:
+            ValueHex = hex(Value)
+            if ValueHex.__len__() <= 4:
+                array = [0x01, SetAddress, ModbusHighAddress, ModbusLowAddress, 0x00, Value]
+                currentValueQ, currentValueH = self.calCrc(array)
+                setValueCmd = [0x01, SetAddress, ModbusHighAddress, ModbusLowAddress, 0x00, Value, currentValueQ,
+                               currentValueH]
+            else:
+                ValueHex = hex(Value)
+                ValueHexQ = int(ValueHex[0] + ValueHex[1] + ValueHex[2], 16)
+                ValueHexH = int(ValueHex[0] + ValueHex[1] + ValueHex[-2] + ValueHex[-1], 16)
+                array = [0x01, SetAddress, ModbusHighAddress, ModbusLowAddress, ValueHexQ, ValueHexH]
+                currentValueQ, currentValueH = self.calCrc(array)
+                setValueCmd = [0x01, SetAddress, ModbusHighAddress, ModbusLowAddress, ValueHexQ, ValueHexH,
+                               currentValueQ,
+                               currentValueH]
+        self.sc.write(setValueCmd)
+        if isReadSerial:
+            back = self.readSerial()
+            value = int.from_bytes(back[3:5], byteorder='big', signed=True)
+            if value < 0:
+                value = value + 1
+            self.sc.flush()
+            return value
+        else:
+            time.sleep(0.005)
+            return
--- RGD-35-14Control.py
+++ RGD-35-14Control.py
@@ -6,206 +6,95 @@ import time
 import crcmod
 import threading
 from tqdm import *
+from ControlRoot import ControlRoot
 
-close_cmd = [0xA0, 0x01, 0x01, 0xA2]
+
+def isRange(value, min_, max_):
+    if not min_ <= value <= max_:
+        raise RuntimeError('Out of range')
 
 
-class ComSwitch(object):
-    def __init__(self, com="/dev/ttyUSB0"):
-        self.sc = serial.Serial(port=com, baudrate=115200)
-        self.crc16 = crcmod.mkCrcFun(0x18005, rev=True, initCrc=0xFFFF, xorOut=0x0000)
-
-    def calCrc(self, array):
-        bytes_ = b''
-        for i in range(array.__len__()):
-            bytes_ = bytes_ + array[i].to_bytes(1, byteorder='little', signed=True)
-        crc = hex(self.crc16(bytes_))
-        print(bytes_)
-        print(crc)
-        crcQ = '0x' + crc[-2] + crc[-1]
-        crcH = '0x' + crc[-4] + crc[-3]
-        return int(crcQ.encode(), 16), int(crcH.encode(), 16)
-
-    def readSerial(self):
-        # BTime = time.time()
-        time.sleep(0.025)
-        readContent = self.sc.read_all()
-        # self.sc.flush()
-        return readContent
+class SetCmd(object):
+    def __init__(self, ControlInstance=ControlRoot()):
+        self.Hand = ControlInstance
 
     # 初始化夹爪
-    def InitFinger(self):
-        init_cmd = [0x01, 0x06, 0x01, 0x00, 0x00, 0x01, 0x49, 0xF6]
-        self.sc.write(init_cmd)
-        print(self.readSerial())
+    def HandInit(self):
+        self.Hand.sendCmd(ModbusHighAddress=0x01, ModbusLowAddress=0x00, Value=1)
 
     # 力值
-    def setForce(self, Force):
-        array = [0x01, 0x06, 0x01, 0x01, 0x00, Force]
-        crcQ, crcH = self.calCrc(array)
-        ForceCmd = [0x01, 0x06, 0x01, 0x01, 0x00, Force, crcQ, crcH]
-        self.sc.write(ForceCmd)
-        print(self.readSerial())
+    def Force(self, value):
+        isRange(value, 20, 100)
+        self.Hand.sendCmd(ModbusHighAddress=0x01, ModbusLowAddress=0x01, Value=value)
 
-    # 读取设定位置
-    def readSetPosition(self):
-        # 01 03 01 03 00 01 75 F6
-        readPositionCmd = [0x01, 0x03, 0x01, 0x03, 0x00, 0x01, 0x75, 0xF6]
-        self.sc.write(readPositionCmd)
-        data = self.readSerial()
-        divData = str(data).split("\\x")
-        PosInt = int((divData[-4] + divData[-3]).encode(), 16)
-        return PosInt
+    # 位置
+    def Position(self, value):
+        isRange(value, 0, 1000)
+        self.Hand.sendCmd(ModbusHighAddress=0x01, ModbusLowAddress=0x03, Value=value)
 
-    # 读取实时位置
-    def readCurrentPosition(self, loop):
-        # 01 03 01 03 00 01 75 F6
-        readPositionCmd = [0x01, 0x03, 0x02, 0x02, 0x00, 0x01, 0x24, 0x72]
-        self.sc.write(readPositionCmd)
-        data = self.readSerial()
-        divData = str(data).split("\\x")
-        PosInt = int((divData[-4] + divData[-3]).encode(), 16)
-        print(PosInt)
-        return PosInt
+    # 速度
+    def Velocity(self, value):
+        isRange(value, 0, 1000)
+        self.Hand.sendCmd(ModbusHighAddress=0x01, ModbusLowAddress=0x04, Value=value)
 
-    # 设置夹爪位置
-    def setPosition(self, Position):
-        PositionHex = hex(Position)
-        if PositionHex.__len__() <= 4:
-            array = [0x01, 0x06, 0x01, 0x03, 0x00, Position]
-            currentPositionQ, currentPositionH = self.calCrc(array)
-            setPositionCmd = [0x01, 0x06, 0x01, 0x03, 0x00, Position, currentPositionQ, currentPositionH]
-        else:
-            PositionHex = hex(Position)
-            PositionHexQ = int(PositionHex[0] + PositionHex[1] + PositionHex[2], 16)
-            PositionHexH = int(PositionHex[0] + PositionHex[1] + PositionHex[-2] + PositionHex[-1], 16)
-            array = [0x01, 0x06, 0x01, 0x03, PositionHexQ, PositionHexH]
-            currentPositionQ, currentPositionH = self.calCrc(array)
-            setPositionCmd = [0x01, 0x06, 0x01, 0x03, PositionHexQ, PositionHexH, currentPositionQ, currentPositionH]
-        self.sc.write(setPositionCmd)
-        print(self.readSerial())
+    # 绝对旋转角度
+    def AbsoluteRotate(self, cmd):
+        isRange(cmd, -32768, 32767)
+        self.Hand.sendCmd(ModbusHighAddress=0x01, ModbusLowAddress=0x05, Value=cmd, isReadSerial=False)
 
-    # 初始化反馈
-    def initFeedback(self):
-        cmd = [0x01, 0x03, 0x02, 0x00, 0x00, 0x01, 0x85, 0xB2]
-        self.sc.write(cmd)
-        print(self.readSerial())
+    # 旋转速度
+    def RotateVelocity(self, value):
+        isRange(value, 1, 100)
+        self.Hand.sendCmd(ModbusHighAddress=0x01, ModbusLowAddress=0x07, Value=value, isReadSerial=False)
 
-    # 夹持状态反馈
-    def clampState(self):
-        cmd = [0x01, 0x03, 0x02, 0x01, 0x00, 0x01, 0xD4, 0x72]
-        self.sc.write(cmd)
-        back = self.readSerial()
+    # 旋转力值
+    def RotateForce(self, value):
+        isRange(value, 20, 100)
+        self.Hand.sendCmd(ModbusHighAddress=0x01, ModbusLowAddress=0x08, Value=value)
+
+    # 相对旋转角度
+    def RelativeRotate(self, cmd):
+        isRange(cmd, -32768, 32767)
+        self.Hand.sendCmd(ModbusHighAddress=0x01, ModbusLowAddress=0x09, Value=cmd)
+
+    def InitFeedback(self):
+        back = self.Hand.sendCmd(ModbusHighAddress=0x02, ModbusLowAddress=0x00, isSet=False)
+        while back == 0:
+            self.HandInit()
+            back = self.Hand.sendCmd(ModbusHighAddress=0x02, ModbusLowAddress=0x00, isSet=False)
+            print(back)
+        while back == 2:
+            time.sleep(0.1)
+            back = self.Hand.sendCmd(ModbusHighAddress=0x02, ModbusLowAddress=0x00, isSet=False)
+            print(back)
+
+
+class ReadStatus(object):
+    def __init__(self, ControlInstance=ControlRoot()):
+        self.Hand = ControlInstance
+
+    def RTRotateAngle(self):
+        back = self.Hand.sendCmd(ModbusHighAddress=0x02, ModbusLowAddress=0x08, isSet=False)
         print(back)
-        back = self.readSerial()
-        divBack = str(back).split("\\x")
-        state = int(divBack[-3].encode(), 16)
-        print(state)
-
-    # IO开关
-    def IOSwitch(self, mode):
-        cmd = [0x01, 0x06, 0x04, 0x02, 0x00, 0x00, 0x29, 0x3A]
-        self.sc.write(cmd)
-        back = self.readSerial()
-
-    # 设置相对旋转位置
-    def setRelativeRotation(self, Rotation):
-        if Rotation < 0:
-            RotationHex = hex(Rotation)
-            if RotationHex.__len__() <= 5:
-                array = [0x01, 0x06, 0x01, 0x09, 0x00, Rotation]
-                currentRotationQ, currentRotationH = self.calCrc(array)
-                setRotationCmd = [0x01, 0x06, 0x01, 0x09, 0x00, Rotation, currentRotationQ, currentRotationH]
-            else:
-                RotationHex = hex(Rotation)
-                RotationHexQ = int(RotationHex[0] + RotationHex[1] + RotationHex[2] + RotationHex[3], 16) - 1
-                RotationHexH = int(RotationHex[0] + RotationHex[1] + RotationHex[2] + RotationHex[-2] + RotationHex[-1],
-                                   16) - 1
-                array = [0x01, 0x06, 0x01, 0x09, RotationHexQ, RotationHexH]
-                currentRotationQ, currentRotationH = self.calCrc(array)
-                setRotationCmd = [0x01, 0x06, 0x01, 0x09, RotationHexQ, RotationHexH, currentRotationQ,
-                                  currentRotationH]
-        else:
-            RotationHex = hex(Rotation)
-            if RotationHex.__len__() <= 4:
-                array = [0x01, 0x06, 0x01, 0x09, 0x00, Rotation]
-                currentRotationQ, currentRotationH = self.calCrc(array)
-                setRotationCmd = [0x01, 0x06, 0x01, 0x09, 0x00, Rotation, currentRotationQ, currentRotationH]
-            else:
-                RotationHex = hex(Rotation)
-                RotationHexQ = int(RotationHex[0] + RotationHex[1] + RotationHex[2], 16)
-                RotationHexH = int(RotationHex[0] + RotationHex[1] + RotationHex[-2] + RotationHex[-1], 16)
-                array = [0x01, 0x06, 0x01, 0x09, RotationHexQ, RotationHexH]
-                currentRotationQ, currentRotationH = self.calCrc(array)
-                setRotationCmd = [0x01, 0x06, 0x01, 0x09, RotationHexQ, RotationHexH, currentRotationQ,
-                                  currentRotationH]
-        self.sc.write(setRotationCmd)
-        print(self.readSerial())
-
-    # 设置绝对旋转位置
-    def setAbsoluteRotation(self, Rotation):
-        if Rotation < 0:
-            RotationHex = hex(Rotation)
-            if RotationHex.__len__() <= 5:
-                array = [0x01, 0x06, 0x01, 0x05, 0x00, Rotation]
-                currentRotationQ, currentRotationH = self.calCrc(array)
-                setRotationCmd = [0x01, 0x06, 0x01, 0x05, 0x00, Rotation, currentRotationQ, currentRotationH]
-            else:
-                RotationHex = hex(Rotation)
-                RotationHexQ = int(RotationHex[0] + RotationHex[1] + RotationHex[2] + RotationHex[3], 16) - 1
-                RotationHexH = int(RotationHex[0] + RotationHex[1] + RotationHex[2] + RotationHex[-2] + RotationHex[-1],
-                                   16) - 1
-                array = [0x01, 0x06, 0x01, 0x05, RotationHexQ, RotationHexH]
-                currentRotationQ, currentRotationH = self.calCrc(array)
-                setRotationCmd = [0x01, 0x06, 0x01, 0x05,
-                                  int('0x' + (RotationHexQ.to_bytes(1, byteorder='big', signed=True).hex()), 16),
-                                  int('0x' + RotationHexH.to_bytes(1, byteorder='big', signed=True).hex(), 16),
-                                  currentRotationQ,
-                                  currentRotationH]
-
-        else:
-            RotationHex = hex(Rotation)
-            if RotationHex.__len__() <= 4:
-                array = [0x01, 0x06, 0x01, 0x05, 0x00, Rotation]
-                currentRotationQ, currentRotationH = self.calCrc(array)
-                setRotationCmd = [0x01, 0x06, 0x01, 0x05, 0x00, Rotation, currentRotationQ, currentRotationH]
-            else:
-                RotationHex = hex(Rotation)
-                RotationHexQ = int(RotationHex[0] + RotationHex[1] + RotationHex[2], 16)
-                RotationHexH = int(RotationHex[0] + RotationHex[1] + RotationHex[-2] + RotationHex[-1], 16)
-                array = [0x01, 0x06, 0x01, 0x05, RotationHexQ, RotationHexH]
-                currentRotationQ, currentRotationH = self.calCrc(array)
-                setRotationCmd = [0x01, 0x06, 0x01, 0x05, RotationHexQ, RotationHexH, currentRotationQ,
-                                  currentRotationH]
-        self.sc.write(setRotationCmd)
-        print(self.readSerial())
-
-    # 设置旋转速度
-    def setRotationVel(self, vel):
-        array = [0x01, 0x06, 0x01, 0x07, 0x00, vel]
-        crcQ, crcH = self.calCrc(array)
-        ForceCmd = [0x01, 0x06, 0x01, 0x07, 0x00, vel, crcQ, crcH]
-        self.sc.write(ForceCmd)
-        print(self.readSerial())
-
-    # 设置旋转力
-    def setRotationForce(self, Force):
-        array = [0x01, 0x06, 0x01, 0x08, 0x00, Force]
-        crcQ, crcH = self.calCrc(array)
-        ForceCmd = [0x01, 0x06, 0x01, 0x08, 0x00, Force, crcQ, crcH]
-        self.sc.write(ForceCmd)
-        print(self.readSerial())
 
 
 if __name__ == "__main__":
-    cs = ComSwitch()
-    cs.InitFinger()
+    CR = ControlRoot()
+    cs = SetCmd(CR)
+    re = ReadStatus(CR)
+    cs.HandInit()
+    cs.InitFeedback()
     time.sleep(0.2)
-    for i in tqdm(range(1000)):
-        # cs.setForce(100)
-        cs.setAbsoluteRotation(-360)
-        time.sleep(1)
-        # cs.setPosition(300)
-        # cs.setForce(20)
-        cs.setAbsoluteRotation(360)
-        time.sleep(1)
+    cs.RotateForce(20)
+    setTime = 3
+    BTime = time.time()
+    num = 0
+    while time.time() - BTime < setTime:
+        cs.AbsoluteRotate(-3600, )
+        re.RTRotateAngle()
+        cs.AbsoluteRotate(360)
+        re.RTRotateAngle()
+        num = num + 1
+    print(setTime / (2 * num))
+    # cs.setPosition(300)
+    # cs.setForce(20)
